38ef44f45cc9dc1c65346f604e4d75e6367f0862
Jorge Mercado <jorgemercado212@gmail.com>
2021-11-09 11:31:23 -0400

    error corregido

--- proyecto_cnb/proyecto_cnb/settings.py
+++ proyecto_cnb/proyecto_cnb/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'proyecto_cnb.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-   git 'default': {
+    'default': {
       
        'ENGINE': 'django.db.backends.sqlite3',
        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
@@ -94,7 +94,7 @@ DATABASES = {
   #      'USER': 'root',
   #      'PASSWORD': '',
   #      'HOST': 'localhost',
-  
+
   #      'PORT': '3306',
   #  }
 
