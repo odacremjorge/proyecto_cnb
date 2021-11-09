545d3aa2491ad100f894a98b5a42f921a1107667
Jorge Mercado <jorgemercado212@gmail.com>
2021-11-09 11:27:30 -0400

    BdD

--- proyecto_cnb/proyecto_cnb/settings.py
+++ proyecto_cnb/proyecto_cnb/settings.py
@@ -82,20 +82,21 @@ WSGI_APPLICATION = 'proyecto_cnb.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-   # 'default': {
+   git 'default': {
       
-   #    'ENGINE': 'django.db.backends.sqlite3',
-   #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   # }
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
 
-    'default': {   
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'exalumnos',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+#    'default': {   
+ #       'ENGINE': 'django.db.backends.mysql',
+  #      'NAME': 'exalumnos',
+  #      'USER': 'root',
+  #      'PASSWORD': '',
+  #      'HOST': 'localhost',
+  
+  #      'PORT': '3306',
+  #  }
 
 }
 
